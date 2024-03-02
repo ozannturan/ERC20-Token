90323d510ce30652b7241fc2e9a1f78f4db36dac
ozanturan <ozann.trn@gmail.com>
2024-03-02 12:29:53 +0300

    Add ERC20 token contract implementation

---
+++ erc-20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract OzanToken is ERC20 {
+    constructor()
+    ERC20("OzanToken", "OZT")
+    {
+        _mint(address, (10 * 10**18));
+    }
+}
